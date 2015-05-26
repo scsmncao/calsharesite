76e3de492ceadcedf679da2d30878ccc8021adc2
Simon Cao <Simon@Simons-MacBook-Pro.local>
2015-05-25 17:18:37 -0700

    signin

--- js/calshare.js
+++ js/calshare.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
+
+	var loginstatus = false;
+
+	$("body").removeClass("preload");
+
 	$(".signup-button").click(function () {
 		$(".signup-after").show();
 		$(".signup-before").hide();
+	});
+
+	$("#login-status").click(function () {
+		if (loginstatus) {
+			$(".login").css("opacity", "0");
+		}
+		else {
+			$(".login").css("opacity", "100");
+		}
+		loginstatus = !loginstatus;
 	});
 });
